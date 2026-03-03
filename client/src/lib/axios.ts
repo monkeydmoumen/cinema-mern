b4f4a6a9c61adce150c34869d45d687e5baff284
monkeydmoumen <mmansouri740@gmail.com>
2026-03-03 17:04:20 +0100

    f

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: 'https://cinema-mern-server.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
